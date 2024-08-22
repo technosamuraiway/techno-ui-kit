1cbaadd18361c78067e584ab92eb92bd6ba4589e
nadzeya <vitserresvit@gmail.com>
2024-08-22 16:34:53 +0300

    feat empty function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import s from './styles/Home.module.scss'
 
-import { SelectBox } from './components'
+import { SelectBox, TextField } from './components'
 import { Button } from './components/ui/button'
 import { Header } from './components/ui/header'
 export default function App() {
   return (
     <>
-      <Header withAuthButtons withNotifications />
+      <Header
+        changeLangHandler={function (): void {
+          throw new Error('Function not implemented.')
+        }}
+        withAuthButtons
+        withNotifications
+      />
       <div className={s.body}>
+        <TextField />
         <Button variant={'primary'}>Click me</Button>
         <SelectBox className={s.select} options={[{ name: 'option1' }, { name: 'option2' }]} />
       </div>
