40b1029895ecc3c89c8ffde9a3f8e9696feb209e
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-08-30 10:41:31 +0300

    feat: added black theme for storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,32 @@
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import '../src/styles/index.scss'
 
-import '../src/styles/index.scss';
+import { themes } from '@storybook/theming'
+import type { Preview } from '@storybook/react'
 
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+}
+
+export default preview
