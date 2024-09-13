1c15912c7b9fe5578dfbc2b818da87b3d14bd40d
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-09-13 13:32:50 +0300

    TEC-27 feat: add components with icons

--- src/components/ui/sideBarItems/SideBarItems.stories.tsx
+++ src/components/ui/sideBarItems/SideBarItems.stories.tsx
@@ -1,7 +1,19 @@
 import { useState } from 'react'
 
 import { ActiveCreateIcon } from '@/assets/icons/sideBar/ActiveCreateIcon'
+import { ActiveFavoritesIcon } from '@/assets/icons/sideBar/ActiveFavoritesIcon'
+import { ActiveHomeIcon } from '@/assets/icons/sideBar/ActiveHomeIcon'
+import { ActiveMessengerIcon } from '@/assets/icons/sideBar/ActiveMessengerIcon'
+import { ActiveProfileIcon } from '@/assets/icons/sideBar/ActiveProfileIcon'
+import { ActiveSearchIcon } from '@/assets/icons/sideBar/ActiveSearchIcon'
+import { ActiveStatisticsIcon } from '@/assets/icons/sideBar/ActiveStatisticsIcon'
 import { DefaultCreateIcon } from '@/assets/icons/sideBar/DefaultCreateIcon'
+import { DefaultFavoritesIcon } from '@/assets/icons/sideBar/DefaultFavoritesIcon'
+import { DefaultHomeIcon } from '@/assets/icons/sideBar/DefaultHomeIcon'
+import { DefaultMessengerIcon } from '@/assets/icons/sideBar/DefaultMessengerIcon'
+import { DefaultProfileIcon } from '@/assets/icons/sideBar/DefaultProfileIcon'
+import { DefaultSearchIcon } from '@/assets/icons/sideBar/DefaultSearchIcon'
+import { DefaultStatisticsIcon } from '@/assets/icons/sideBar/DefaultStatisticsIcon'
 import { Meta, StoryObj } from '@storybook/react'
 
 import { NavItem, SideBarItems } from './SideBarItems'
@@ -16,37 +28,76 @@ const meta: Meta<typeof SideBarItems> = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-const navItemsArray: NavItem[] = [
+const navFirstItemsArray: NavItem[] = [
   {
-    activeIconComponent: <ActiveCreateIcon />,
-    altText: 'text',
-    defaultIconComponent: <DefaultCreateIcon />,
-    hrefLink: '#create',
+    activeIconComponent: <ActiveHomeIcon />,
+    altText: 'Home Icon',
+    defaultIconComponent: <DefaultHomeIcon />,
+    hrefLink: '#Home',
     id: 1,
     isDisabled: false,
-    text: 'create',
+    text: 'Home',
   },
   {
     activeIconComponent: <ActiveCreateIcon />,
-    altText: 'text',
+    altText: 'Create Icon',
     defaultIconComponent: <DefaultCreateIcon />,
-    hrefLink: '#home',
+    hrefLink: '#Create',
     id: 2,
     isDisabled: false,
-    text: 'home',
+    text: 'Create',
   },
   {
-    activeIconComponent: <ActiveCreateIcon />,
-    altText: 'text',
-    defaultIconComponent: <DefaultCreateIcon />,
-    hrefLink: '#disabled',
+    activeIconComponent: <ActiveProfileIcon />,
+    altText: 'My Profile',
+    defaultIconComponent: <DefaultProfileIcon />,
+    hrefLink: '#My Profile',
     id: 3,
+    isDisabled: false,
+    text: 'My Profile',
+  },
+  {
+    activeIconComponent: <ActiveMessengerIcon />,
+    altText: 'Messenger',
+    defaultIconComponent: <DefaultMessengerIcon />,
+    hrefLink: '#Messenger',
+    id: 4,
+    isDisabled: false,
+    text: 'Messenger',
+  },
+  {
+    activeIconComponent: <ActiveSearchIcon />,
+    altText: 'Search',
+    defaultIconComponent: <DefaultSearchIcon />,
+    hrefLink: '#Search',
+    id: 5,
     isDisabled: true,
-    text: 'disabled',
+    text: 'Search',
   },
 ]
 
-export const SideBarItemsWrapper = () => {
+const navSecondItemsArray: NavItem[] = [
+  {
+    activeIconComponent: <ActiveStatisticsIcon />,
+    altText: 'Statistics Icon',
+    defaultIconComponent: <DefaultStatisticsIcon />,
+    hrefLink: '#Statistics',
+    id: 10,
+    isDisabled: false,
+    text: 'Statistics',
+  },
+  {
+    activeIconComponent: <ActiveFavoritesIcon />,
+    altText: 'Favorites Icon',
+    defaultIconComponent: <DefaultFavoritesIcon />,
+    hrefLink: '#Favorites',
+    id: 11,
+    isDisabled: false,
+    text: 'Favorites',
+  },
+]
+
+const SideBarItemsWrapper = () => {
   const [currentPath, setCurrentPath] = useState<string>('')
 
   const activeConditionFunction = (itemPath: string) => {
@@ -58,14 +109,21 @@ export const SideBarItemsWrapper = () => {
   }
 
   return (
-    <SideBarItems
-      activeCondition={activeConditionFunction}
-      navItems={navItemsArray}
-      onSideBarItemClick={onSideBarItemClickHandler}
-    />
+    <>
+      <SideBarItems
+        activeCondition={activeConditionFunction}
+        navItems={navFirstItemsArray}
+        onSideBarItemClick={onSideBarItemClickHandler}
+      />
+      <SideBarItems
+        activeCondition={activeConditionFunction}
+        navItems={navSecondItemsArray}
+        onSideBarItemClick={onSideBarItemClickHandler}
+      />
+    </>
   )
 }
 
-export const DefaultSideBarItems: Story = {
+export const ExampleOfSideBarItems: Story = {
   render: () => <SideBarItemsWrapper />,
 }
