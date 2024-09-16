adc4bd6e095961b1b1ade0d4e9d342dcafc6958e
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-09-16 13:44:08 +0300

    fix: modal refactor

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,14 +1,14 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
-import { CloseIcon } from '@/assets/icons/closeIcon'
+import { CloseIcon } from '@/assets/icons/CloseIcon'
 import { Button, ButtonVariant, Typography } from '@/components'
 import * as Dialog from '@radix-ui/react-dialog'
 import clsx from 'clsx'
 
 import s from './Modal.module.scss'
 
-// 'L' - 644px; 'M' - 492px(486px); 'S' - 378px; 'XL' - 972px; 'XS' - 330px
-export type ModalSize = 'L' | 'M' | 'S' | 'XL' | 'XS'
+// 'L' - 644px; 'M' - 492px(486px); 'S' - 438px; 'XL' - 972px; 'XS' - 378px; 'XXS' - 330px
+export type ModalSize = 'L' | 'M' | 'S' | 'XL' | 'XS' | 'XXS'
 
 interface IProps extends ComponentPropsWithoutRef<typeof Dialog.Root> {
   children: ReactNode
@@ -47,21 +47,23 @@ export const Modal = ({
       )}
 
       <Dialog.Portal>
-        <Dialog.Overlay className={clsx(s.overlay, overlayClassName)}>
-          <Dialog.Content className={clsx(s.content, s[modalSize], contentClassName)}>
-            <div className={clsx(s.header, headerClassName)}>
-              <Dialog.Title asChild>
-                <Typography as={'h1'} variant={'h1'}>
-                  {headerTitle}
-                </Typography>
-              </Dialog.Title>
-              <Dialog.Close className={clsx(s.closeButton, closeButtonClassName)}>
-                <CloseIcon />
-              </Dialog.Close>
-            </div>
-            {children}
-          </Dialog.Content>
-        </Dialog.Overlay>
+        <Dialog.Overlay className={clsx(s.overlay, overlayClassName)} />
+        <Dialog.Content
+          aria-describedby={undefined}
+          className={clsx(s.content, s[modalSize], contentClassName)}
+        >
+          <div className={clsx(s.header, headerClassName)}>
+            <Dialog.Title asChild>
+              <Typography as={'h1'} variant={'h1'}>
+                {headerTitle}
+              </Typography>
+            </Dialog.Title>
+            <Dialog.Close className={clsx(s.closeButton, closeButtonClassName)}>
+              <CloseIcon />
+            </Dialog.Close>
+          </div>
+          {children}
+        </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   )
