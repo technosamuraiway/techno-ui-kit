c9f976741937b672154c59f33a07340b6c4adada
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-09-23 07:42:21 +0300

    TEC-47 feat: dropdown start - 2

--- src/components/ui/dropDown/DropDown.tsx
+++ src/components/ui/dropDown/DropDown.tsx
@@ -6,25 +6,37 @@ import { clsx } from 'clsx'
 import s from './DropDown.module.scss'
 
 export type DropdownProps = {
+  arrowCN?: string
+  contentAlign?: 'center' | 'end' | 'start'
+  contentCN?: string
   style?: CSSProperties
   trigger: ReactNode
-  triggerClassName?: string
+  triggerCN?: string
 } & ComponentPropsWithRef<typeof D.Root>
 
-const Root = ({ children, style, trigger, triggerClassName, ...rest }: DropdownProps) => {
+const Root = ({
+  arrowCN,
+  children,
+  contentAlign = 'end',
+  contentCN,
+  style,
+  trigger,
+  triggerCN,
+  ...rest
+}: DropdownProps) => {
   return (
     <D.Root {...rest}>
       <D.Trigger asChild>
-        <button className={clsx(triggerClassName, s.trigger)} style={style} type={'button'}>
+        <button className={clsx(s.trigger, triggerCN)} style={style} type={'button'}>
           {trigger}
         </button>
       </D.Trigger>
 
       <D.Portal>
-        <D.Content align={'end'} className={s.content} sideOffset={5}>
+        <D.Content align={contentAlign} className={clsx(s.content, contentCN)} sideOffset={5}>
           {children}
           <D.Arrow asChild>
-            <div className={s.arrow} />
+            <div className={clsx(s.arrow, arrowCN)} />
           </D.Arrow>
         </D.Content>
       </D.Portal>
@@ -34,16 +46,19 @@ const Root = ({ children, style, trigger, triggerClassName, ...rest }: DropdownP
 
 type ItemProps = {
   children: ReactNode
-  className?: string
 } & ComponentPropsWithRef<typeof D.Item>
 
 const Item = ({ children, className, ...rest }: ItemProps) => (
-  <D.Item {...rest} className={clsx(s.item, className)}>
+  <D.Item className={clsx(s.item, className)} {...rest}>
     {children}
   </D.Item>
 )
 
-const Separator = () => <D.Separator className={s.separator} />
+type SeparatorProps = {} & ComponentPropsWithRef<typeof D.Separator>
+
+const Separator = ({ className, ...rest }: SeparatorProps) => (
+  <D.Separator className={clsx(s.separator, className)} {...rest} />
+)
 
 export const Dropdown = {
   Item,
