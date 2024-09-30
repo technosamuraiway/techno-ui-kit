4eeec616d7e1fd48d8b8b0076964284a724561bf
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-09-30 15:45:54 +0300

    fix: tabs fix - 2

--- src/components/ui/tabs/Tabs.stories.tsx
+++ src/components/ui/tabs/Tabs.stories.tsx
@@ -13,23 +13,17 @@ const tabsData: TabType[] = [
 ]
 
 // Template for stories
-/** isWithContent пропс только для истории, чтобы отключить контент, при использовании компоненты, просто
- * не нужно добавлять children */
-const Template: StoryFn<{ isWithContent: boolean } & TabsProps> = ({ isWithContent, ...args }) => (
+const Template: StoryFn<TabsProps> = args => (
   <Tabs.Root {...args}>
-    {isWithContent && (
-      <>
-        <Tabs.Content value={'tab1'}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda deserunt
-          dolore, doloribus, eius est hic iusto laboriosam molestiae optio similique vel voluptatum.
-          Ab beatae deserunt distinctio doloremque ipsam molestias perferendis vero voluptates
-          voluptatibus. Aut debitis dolore dolorum, eos error incidunt laboriosam nam
-          necessitatibus, obcaecati officia quam quis sapiente sint?
-        </Tabs.Content>
-        <Tabs.Content value={'tab2'}>Content 2</Tabs.Content>
-        <Tabs.Content value={'tab3'}>Disabled</Tabs.Content>
-      </>
-    )}
+    <Tabs.Content value={'tab1'}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda deserunt dolore,
+      doloribus, eius est hic iusto laboriosam molestiae optio similique vel voluptatum. Ab beatae
+      deserunt distinctio doloremque ipsam molestias perferendis vero voluptates voluptatibus. Aut
+      debitis dolore dolorum, eos error incidunt laboriosam nam necessitatibus, obcaecati officia
+      quam quis sapiente sint?
+    </Tabs.Content>
+    <Tabs.Content value={'tab2'}>Content 2</Tabs.Content>
+    <Tabs.Content value={'tab3'}>Disabled</Tabs.Content>
   </Tabs.Root>
 )
 
@@ -49,7 +43,6 @@ export default {
 export const DefaultTabs = Template.bind({})
 DefaultTabs.args = {
   defaultValue: tabsData[0].value,
-  isWithContent: false,
   onValueChange: fn(),
   tabs: tabsData,
 }
@@ -57,7 +50,6 @@ DefaultTabs.args = {
 export const NotFullWidth = Template.bind({})
 NotFullWidth.args = {
   defaultValue: tabsData[0].value,
-  isWithContent: false,
   notFullWidth: true,
   onValueChange: fn(),
   tabs: tabsData,
@@ -67,7 +59,6 @@ NotFullWidth.args = {
 export const WithContent = Template.bind({})
 WithContent.args = {
   defaultValue: tabsData[0].value,
-  isWithContent: true,
   onValueChange: fn(),
   tabs: tabsData,
 }
