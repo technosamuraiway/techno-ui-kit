aa11876f995ad9243b59eb6a57e467a5d7645404
max1k1 <63220372+max1k1@users.noreply.github.com>
2024-11-01 17:59:11 +0200

    fix: using radioGroup instead of radioButton

---
+++ src/components/ui/radioGroup/RadioGroup.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ComponentProps, useState } from 'react'
+
+import { RadioGroup } from './RadioGroup'
+
+const options = [
+  { id: 'radio1', label: 'Option 1', value: 'option1' },
+  { id: 'radio2', label: 'Option 2', value: 'option2' },
+  { id: 'radio3', label: 'Option 3', value: 'option3' },
+]
+
+const meta: Meta<typeof RadioGroup> = {
+  argTypes: {
+    onValueChange: { action: 'value changed' },
+  },
+  component: RadioGroup,
+  tags: ['autodocs'],
+  title: 'Components/RadioGroup',
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const Template = (args: ComponentProps<typeof RadioGroup>) => {
+  const [selectedValue, setSelectedValue] = useState(args.value || options[0].value)
+
+  const handleValueChange = (value: string) => {
+    setSelectedValue(value)
+    if (args.onValueChange) {
+      args.onValueChange(value)
+    }
+  }
+
+  return <RadioGroup {...args} onValueChange={handleValueChange} value={selectedValue} />
+}
+
+export const DefaultRadioGroup: Story = {
+  args: {
+    disabled: false,
+    options,
+    value: options[0].value, // Initial selected value
+  },
+  render: Template,
+}
+
+export const DisabledRadioGroup: Story = {
+  args: {
+    disabled: true,
+    options,
+    value: options[0].value,
+  },
+  render: (args: ComponentProps<typeof RadioGroup>) => <RadioGroup {...args} />,
+}
---
+++ src/components/ui/radioGroup/RadioGroup.tsx
@@ -0,0 +1,46 @@
+import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react'
+
+import * as Radio from '@radix-ui/react-radio-group'
+
+import s from './RadioGroup.module.scss'
+
+type Options = {
+  id: string
+  label: string
+  value: string
+}
+
+export type RadioGroupProps = {
+  disabled?: boolean
+  onValueChange: (value: string) => void
+  options: Options[]
+  value: string
+} & ComponentPropsWithRef<typeof Radio.Root>
+
+export const RadioGroup = forwardRef<ElementRef<typeof Radio.Root>, RadioGroupProps>(
+  ({ disabled = false, onValueChange, options, value, ...rest }, ref) => {
+    return (
+      <Radio.Root
+        className={s.radioRoot}
+        {...rest}
+        disabled={disabled}
+        onValueChange={onValueChange}
+        ref={ref}
+        value={value}
+      >
+        {options.map(el => (
+          <div className={s.radioItemWrapper} key={el.id}>
+            <Radio.Item className={s.radioItem} disabled={disabled} id={el.id} value={el.value}>
+              <Radio.Indicator className={s.RadioGroupIndicator} />
+            </Radio.Item>
+            <label className={`${s.radioLabel} ${disabled ? s.disabledLabel : ''}`} htmlFor={el.id}>
+              {el.label}
+            </label>
+          </div>
+        ))}
+      </Radio.Root>
+    )
+  }
+)
+
+RadioGroup.displayName = 'RadioGroup'
