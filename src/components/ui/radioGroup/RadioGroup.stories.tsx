ff029e86ec2aadb790248c355b12c19f40d3a1fa
max1k1 <63220372+max1k1@users.noreply.github.com>
2024-11-02 20:04:26 +0200

    fix: correct RadioGroup options

--- src/components/ui/radioGroup/RadioGroup.stories.tsx
+++ src/components/ui/radioGroup/RadioGroup.stories.tsx
@@ -5,9 +5,9 @@ import { ComponentProps, useState } from 'react'
 import { RadioGroup } from './RadioGroup'
 
 const options = [
-  { id: 'radio1', label: 'Option 1', value: 'option1' },
-  { id: 'radio2', label: 'Option 2', value: 'option2' },
-  { id: 'radio3', label: 'Option 3', value: 'option3' },
+  { label: 'Option 1', value: 'option1' },
+  { label: 'Option 2', value: 'option2' },
+  { label: 'Option 3', value: 'option3' },
 ]
 
 const meta: Meta<typeof RadioGroup> = {
